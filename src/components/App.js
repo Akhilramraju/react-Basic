1021005d967a17225aac1e25e51b034ee59df3bc
Akhil Ram Raju Nannapuraju <akhil8099123@gmail.com>
2021-02-08 12:20:18 +1100

    finished routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,22 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
-import {Route} from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
+import {Route,Switch,Redirect} from "react-router-dom";
 
 function App(){
    return (
        <div className={"container-fluid"}>
            <Header />
+           <Switch>
            <Route path={"/"} exact  component={HomePage}/>
-           <Route path={"/courses"} component={CoursesPage}/>
+           <Route path={"/courses"} component={CoursesPage }/>
            <Route path={"/about"} component={AboutPage}/>
+           <Route path={"/course/:slug"} component={ManageCoursePage} />
+           <Redirect from={"/about-page"} to={"/about"}></Redirect>
+           <Route component={NotFoundPage}></Route>
+           </Switch>
 
        </div>
    );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
  function HomePage(){
      return (
      <div className={"jumbotron"}>
          <h1>Pluralsight Admin</h1>
          <p>React</p>
-         <a href={"/about"}> About</a>
+         <Link to={"about"} className={"btn btn-primary"}>
+             About
+         </Link>
      </div>
      );
  }
