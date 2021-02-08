17b6fa4ce699f6da0689c8dc043c0d12cea0515a
Akhil Ram Raju Nannapuraju <akhil8099123@gmail.com>
2021-02-08 22:53:12 +1100

    added form basic

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,22 +1,18 @@
 import React from "react";
+import TextInput from "./common/TextInput";
 
 function CourseForm(props){
     return(
 
         <form>
-            <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <div className="field">
-                    <input
-                        id="title"
-                        type="text"
-                        onChange={props.onChange}
-                        name="title"
-                        className="form-control"
-                        value={props.course.title || ""}
-                    />
-                </div>
-            </div>
+           <TextInput
+               id={"title"}
+               label={"title"}
+               onChange={props.onChange}
+               name={"title"}
+               value={props.course.category}
+
+           />
 
             <div className="form-group">
                 <label htmlFor="author">Author</label>
@@ -35,19 +31,14 @@ function CourseForm(props){
                 </div>
             </div>
 
-            <div className="form-group">
-                <label htmlFor="category">Category</label>
-                <div className="field">
-                    <input
-                        type="text"
-                        id="category"
-                        name="category"
-                        onChange={props.onChange}
-                        className="form-control"
-                        value={props.course.category || ""}
-                    />
-                </div>
-            </div>
+            <TextInput
+                id={"category"}
+                label={"category"}
+                onChange={props.onChange}
+                name={"category"}
+                value={props.course.category}
+
+            />
 
             <input type="submit" value="Save" className="btn btn-primary" />
         </form>
